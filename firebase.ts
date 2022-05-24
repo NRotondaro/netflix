5a1a72ef29357c9b4f4521bc5b99a3f88103e6ae
NRotondaro <niksonrotondaro1@gmail.com>
2022-05-23 23:56:42 -0300

    feat(login): created the mobile first login interface

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBH1lhQ1arAvaPAIRbjjh5qWPmVPDVdTXE',
+  authDomain: 'netflix-265ec.firebaseapp.com',
+  projectId: 'netflix-265ec',
+  storageBucket: 'netflix-265ec.appspot.com',
+  messagingSenderId: '393652851471',
+  appId: '1:393652851471:web:95ce4f0ba7524b97bf647a',
+}
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+
+export default app
+export { auth, db }
